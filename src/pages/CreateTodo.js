34abfecc523d382fbe0e70740585f7c2c895abd0
gaurangkeluskar22 <Gaurang.Keluskar@ril.com>
2024-03-08 10:46:21 +0530

    added code

---
+++ src/pages/CreateTodo.js
@@ -0,0 +1,52 @@
+import { Box, Button, Typography } from '@mui/material'
+import {useState, useEffect} from 'react'
+
+const CreateTodo = () =>{
+    const [todoList, setTodoList] = useState([])
+    const [todo, setTodo] = useState('')
+
+    const fetchTodoList = async () =>{
+
+    }
+
+    useEffect(()=>{
+        fetchTodoList()
+    },[])
+
+    const createTodo = (e) => {
+        setTodoList((prevState)=>[
+            ...prevState,
+            todo
+        ])
+    }
+
+    const handleInputChange = (e) => {
+        const input = e?.target?.value
+        setTodo(input)
+    }
+
+    console.log("t:",todoList)
+    return(
+        <Box sx={{marginTop:'10vh', width:'21vw'}}>
+            <Box sx={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>   
+                 <input type='text' onChange={handleInputChange}/>
+                 <Button variant='contained' onClick={createTodo}>Create</Button>
+            </Box>
+            <Box sx={{paddingTop:'10vh'}}>
+                {
+                    todoList?.map((item, index)=>{
+                     return(
+                        <Box sx={{display:'flex', flexDirection:'row', margin:'5px', alignItems:'center', justifyContent:'center'}} key={index}>
+                            <Typography sx={{width:'15vw', alignItems:'center'}}>{item}</Typography>
+                            <Button variant='contained' color='error'>Delete</Button>
+                        </Box>
+                     )  
+                    })
+                }
+                
+            </Box>
+        </Box>
+    )
+}
+
+export default CreateTodo
